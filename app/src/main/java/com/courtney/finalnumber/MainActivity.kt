ed7e311030b391eea31d4bb79953700ad4d40bf1
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-04-21 22:34:26 +0800

    version 1
    version 1

--- app/src/main/java/com/courtney/finalnumber/MainActivity.kt
+++ app/src/main/java/com/courtney/finalnumber/MainActivity.kt
@@ -1,14 +1,22 @@
 package com.courtney.finalnumber
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
+
+    val secretNumber = SecretNumber()
+    private val TAG = MainActivity::class.java.simpleName
+    var min = 1
+    var max = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +24,64 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            AlertDialog.Builder(this)
+                .setTitle("Restart")
+                .setMessage("Restart?")
+                .setPositiveButton("OK") { dialog, which ->
+                    min = 1
+                    max = 100
+                    secretNumber.restart()
+                    txt_count.text = secretNumber.count.toString()
+                    txt_secret.text = secretNumber.secret.toString()
+                    edt_secret.setText("")
+                    Log.d(TAG, "secret: ${secretNumber.secret}")
+                }
+                .setNeutralButton("Cancel", null)
+                .show()
         }
+
+        txt_secret.text = secretNumber.secret.toString()
+        info { "secret: ${secretNumber.secret}" }
+
+        btn_guess.setOnClickListener {
+            val n = edt_secret.text.toString().toInt()
+            info { "n: $n" }
+            check(n)
+        }
+
+    }
+
+    private fun check(n: Int) {
+
+        val diff = secretNumber.validate(n)
+        var message = "Bingo! The number is ${secretNumber.secret}"
+
+        if (n in min until max) {
+            when {
+                diff > 0 -> {
+                    message = "Smaller! $min ~ $n"
+                    max = n
+                }
+
+                diff < 0 -> {
+                    message = "Bigger! $n ~ $max"
+                    min = n
+                }
+            }
+
+            txt_count.text = secretNumber.count.toString()
+
+        } else {
+            message = "You enter a wrong number! Please enter number in range $min ~ $max"
+        }
+
+        AlertDialog.Builder(this)
+            .setTitle("Message")
+            .setMessage(message)
+            .setPositiveButton("OK", null)
+            .show()
+
+        edt_secret.setText("")
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/courtney/finalnumber/SecretNumber.kt
+++ app/src/main/java/com/courtney/finalnumber/SecretNumber.kt
@@ -1,4 +1,28 @@
 package com.courtney.finalnumber
 
+import kotlin.random.Random
+
 class SecretNumber {
+
+    var secret = Random.nextInt(100) + 1
+    var count = 0
+
+    fun validate (number: Int): Int {
+        count ++
+        return number - secret
+    }
+
+    fun restart () {
+        secret = Random.nextInt(100) + 1
+        count = 0
+    }
+
+}
+
+fun main () {
+
+    val secretNumber = SecretNumber()
+    println(secretNumber.secret)
+    println("${secretNumber.validate(2)}, count: ${secretNumber.count}")
+
 }

79e1d1618591b78918dcc25548caecf45d1d5f8a
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-05-02 01:12:55 +0800

    Big Change
    Big Change

--- app/src/main/java/com/courtney/finalnumber/MainActivity.kt
+++ app/src/main/java/com/courtney/finalnumber/MainActivity.kt
@@ -3,116 +3,62 @@ package com.courtney.finalnumber
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.alert
-import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity(), AnkoLogger {
 
-    val secretNumber = SecretNumber()
-    var min = 1      // 最小之範圍
-    var max = 100    // 最大之範圍
+    var secretNumber = SecretNumber(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        // 重玩
         fab.setOnClickListener {
-            AlertDialog.Builder(this)
-                .setTitle(getString(R.string.restart))
-                .setMessage(getString(R.string.are_you_sure_to_restart))
-                .setPositiveButton(getString(R.string.ok)) { _, _ ->
-                    replay()
-                }
-                .show()
+            replay()
         }
 
-        // 設定秘密數字在畫面上
         txt_secret.text = secretNumber.secret.toString()
-        info { "secret: ${secretNumber.secret}" }
-
-        // 猜數字
+        txt_best_record.text = secretNumber.bestDisplay
         btn_guess.setOnClickListener {
-            val n = edt_secret.text.toString().toInt()
-            info { "n: $n" }
-            check(n)
+            secretNumber.number = edt_secret.text.toString().toInt()
+            if (secretNumber.number in secretNumber.min until secretNumber.max + 1) {
+                if (secretNumber.different(secretNumber.number) == 0) {
+                    alert {
+                        title = getString(R.string.guess)
+                        message = secretNumber.message
+                        positiveButton(getString(R.string.ok)) {
+                            replay()
+                        }
+                    }.show()
+                } else {
+                    edt_secret.text = null
+                    txt_count.text = secretNumber.count.toString()
+                    txt_hint.text = secretNumber.hint
+                    toast(secretNumber.message)
+                }
+            } else {
+                alert {
+                    title = getString(R.string.guess)
+                    txt_count.text = secretNumber.count.toString()
+                    edt_secret.text = null
+                    message = secretNumber.message
+                    positiveButton(getString(R.string.ok)) {}
+                }.show()
+            }
         }
-
     }
 
     private fun replay() {
-        min = 1
-        max = 100
         secretNumber.restart()
-        txt_count.text = secretNumber.count.toString()
         txt_secret.text = secretNumber.secret.toString()
-        // 清空填數字欄位
-        edt_secret.setText("")
-        info { "secret: ${secretNumber.secret}" }
-    }
-
-    private fun check(n: Int) {
-
-        val diff = secretNumber.validate(n)
-        var message: String
-        var hint = "1  ${getString(R.string.to)} 100"
-
         txt_count.text = secretNumber.count.toString()
-
-        if (n in min until max + 1) {
-            when {
-                // 三次內猜對
-                diff == 0 && secretNumber.count < 3 -> {
-                    message = "${getString(R.string.excellent_the_number_is)} ${secretNumber.secret}"
-
-                    alert(message, getString(R.string.message)) {
-                        positiveButton(getString(R.string.ok)) { replay() }
-                    }.show()
-
-                }
-                // 猜的數字大於秘密數字
-                diff > 0 -> {
-                    message = "${getString(R.string.smaller)}  $min ${getString(R.string.to)} $n"
-                    hint = "$min ${getString(R.string.to)} $n"
-                    max = n
-                    toast(message)
-                }
-                // 猜的數字小於秘密數字
-                diff < 0 -> {
-                    message = getString(R.string.bigger) + n + "\t" + getString(R.string.to) + "\t" + max
-                    hint = "$n  ${getString(R.string.to)} $max"
-                    min = n
-                    toast(message)
-                }
-                // 超過三次猜對
-                else -> {
-                    message = "${getString(R.string.bingo_the_number_is)} ${secretNumber.secret}"
-
-                    alert(message, getString(R.string.message)) {
-                        positiveButton(getString(R.string.ok)) { replay() }
-                    }.show()
-                }
-            }
-
-            txt_hint.text = hint
-            txt_count.text = secretNumber.count.toString()
-
-        } else {
-
-            message =
-                "${getString(R.string.you_enter_a_wrong_number_Please_enter_number_in_range)} $min ${getString(R.string.to)} $max"
-            toast(message)
-        }
-
-        // 清空填數字欄位
-        edt_secret.setText("")
+        edt_secret.text = null
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/courtney/finalnumber/SecretNumber.kt
+++ app/src/main/java/com/courtney/finalnumber/SecretNumber.kt
@@ -1,28 +1,78 @@
 package com.courtney.finalnumber
 
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
 import kotlin.random.Random
 
-class SecretNumber {
+class SecretNumber(val context: Context) {
 
-    var secret = Random.nextInt(100) + 1
-    var count = 0
 
-    fun validate (number: Int): Int {
-        count ++
+    var secret: Int = Random.nextInt(100) + 1  // 秘密數字
+    var count = 0                                    // 猜的次數
+    lateinit var message: String                     // 訊息
+    lateinit var hint: String                        // 提示訊息
+    var max: Int = 100                               // 最大值
+    var min: Int = 0                                 // 最小值
+    private var best = -1                            // 獲取最佳紀錄
+    var bestDisplay: String = "No Record"
+        get() {
+            // TODO: 不會立即更新
+            best = context.getSharedPreferences("guess", MODE_PRIVATE)
+                .getInt("BEST", -1)
+            field = if (best == -1) "No Record" else best.toString()
+            return field
+        }
+
+    // 判斷輸入之數字 & 判定是否為最佳紀錄
+    var number: Int = 0
+        set(value) {
+            count++
+            field = value
+            if (number in min until max + 1) {
+                message = when {
+                    value > secret -> context.getString(R.string.smaller, min.toString(), number.toString())
+                    value < secret -> context.getString(R.string.bigger, number.toString(), max.toString())
+                    value == secret && count < 3 -> context.getString(R.string.excellent_the_number_is, secret.toString())
+                    else -> {
+                        if (best == -1 || count < best) {
+                            context.getSharedPreferences("guess", MODE_PRIVATE)
+                                .edit()
+                                .putInt("BEST", count)
+                                .apply()
+                        }
+                        context.getString(R.string.bingo_the_number_is, secret.toString())
+                    }
+                }
+
+                hint = when {
+                    value > secret -> {
+                        max = number
+                        "$min ${context.getString(R.string.to)} $number"
+                    }
+                    value < secret -> {
+                        min = number
+                        "$number  ${context.getString(R.string.to)} $max"
+                    }
+                    else -> {
+                        min = 1
+                        max = 100
+                        "$min  ${context.getString(R.string.to)} $max"
+                    }
+                }
+            } else {
+                message = "${context.getString(R.string.you_enter_a_wrong_number_Please_enter_number_in_range)} $min ${context.getString(R.string.to)} $max"
+            }
+        }
+
+    // 計算數字之差
+    fun different(number: Int) : Int {
         return number - secret
     }
 
-    fun restart () {
+    // 重玩
+    fun restart() {
         secret = Random.nextInt(100) + 1
         count = 0
+        hint = "1  ${context.getString(R.string.to)} 100"
     }
-
-}
-
-fun main () {
-
-    val secretNumber = SecretNumber()
-    println(secretNumber.secret)
-    println("${secretNumber.validate(2)}, count: ${secretNumber.count}")
-
 }
